b6612c7f02dbc89d5431cbeb33e200fc7dd2c7c3
MuhammetDilmac <iletisim@muhammetdilmac.com.tr>
2016-11-19 14:57:24 +0300

    frameguardhandler_spec file remove TestNext

--- spec/frameguardhandler_spec.cr
+++ spec/frameguardhandler_spec.cr
@@ -3,51 +3,39 @@ require "./spec_helper"
 describe Helmet::FrameGuardHandler do
   it "sets the header to SAMEORIGIN with no arguments" do
     context = test_context
-    next_handler = TestNext.new
 
     handler = Helmet::FrameGuardHandler.new
-    handler.next = next_handler
     handler.call(context)
 
     context.response.headers["X-Frame-Options"].should eq("SAMEORIGIN")
-    next_handler.called.should be_true
   end
 
   it "sets the header to DENY" do
     context = test_context
-    next_handler = TestNext.new
 
     nowhere = Helmet::FrameGuardHandler::Origin::Nowhere
     handler = Helmet::FrameGuardHandler.new allow_from: nowhere
-    handler.next = next_handler
     handler.call(context)
 
     context.response.headers["X-Frame-Options"].should eq("DENY")
-    next_handler.called.should be_true
   end
 
   it "sets the header to SAMEORIGIN" do
     context = test_context
-    next_handler = TestNext.new
 
     sameorigin = Helmet::FrameGuardHandler::Origin::Same
     handler = Helmet::FrameGuardHandler.new allow_from: sameorigin
-    handler.next = next_handler
     handler.call(context)
 
     context.response.headers["X-Frame-Options"].should eq("SAMEORIGIN")
-    next_handler.called.should be_true
   end
 
   it "sets the header to ALLOW-FROM" do
     context = test_context
-    next_handler = TestNext.new
 
     handler = Helmet::FrameGuardHandler.new allow_from: "http://example.com"
-    handler.next = next_handler
     handler.call(context)
 
     context.response.headers["X-Frame-Options"].should eq("ALLOW-FROM http://example.com")
-    next_handler.called.should be_true
   end
 end
